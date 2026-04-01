598a70653917042eed9123b0b29343edaef9a0e0
fayzan101 <faizanbasheer38@example.com>
2026-04-01 17:56:14 +0500

    Init

--- src/hooks/fees/useFeeScales.ts
+++ src/hooks/fees/useFeeScales.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllFeeScale } from "../../services/fees.service";
-import type { GetAllFeeScaleResponse } from "../types/fees.types";
+import type { GetAllFeeScaleResponse } from "../../types/fees.types";
 
 export function useFeeScales() {
   return useQuery<GetAllFeeScaleResponse>({
